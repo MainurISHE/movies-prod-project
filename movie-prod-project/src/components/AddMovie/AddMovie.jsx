b4f2c74877c3cd8db90bb9f6936fbb22e5238137
Mainur <kentykimainur@gmail.com>
2026-02-02 21:12:14 +0600

    add component AddMovie

---
+++ movie-prod-project/src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function AddMovie({ onAdd }) {
+  const [value, setValue] = useState("");
+
+  const handleAdd = () => {
+    if (!value.trim()) return;
+    onAdd(value);
+    setValue("");
+  };
+
+  return (
+    <div>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Введи название Фильма"
+      />
+      <button onClick={handleAdd}>Добавить</button>
+    </div>
+  );
+}
